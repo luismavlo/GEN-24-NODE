f737f4516e5b9151f4a3d68250e1833d78159bf1
luismavlo <lmavendano1597@gmail.com>
2023-05-25 10:44:38 -0500

    clase 3

--- REPASO/cafeApp/src/app.js
+++ REPASO/cafeApp/src/app.js
@@ -1,11 +1,34 @@
 const express = require("express");
 
 const app = express();
+//este middleware de aca me sirve para que mi servidor
+//entienda formatos json que le estan llegando
+app.use(express.json());
 
-app.get("/saludar", (req, res) => {
-  console.log("me ejecute");
-  res.send("Hello World");
+app.get("/api/v1/products", (req, res) => {
+  res.json({
+    message: "Hello from the get products",
+  });
 });
+
+app.post("/api/v1/products", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    message: "Hello from the post products",
+    product: req.body,
+  });
+});
+
+app.get("/api/v1/products/:id", (req, res) => {
+  res.json({
+    message: "Hello from the get one products",
+  });
+});
+
+app.patch;
+
+app.delete;
 
 app.listen(3000, () => {
   console.log("Server running on port 3000! 🐱‍🐉");
