e1d8b445eed73256f36bb281edd1e9e657ad3154
luismavlo <lmavendano1597@gmail.com>
2023-05-26 20:20:23 -0500

    routes m

--- REPASO/cafeApp/src/app.js
+++ REPASO/cafeApp/src/app.js
@@ -16,41 +16,7 @@ const helloMiddleware = (req, res, next) => {
 
 app.use(helloMiddleware);
 
-const createProduct = (req, res) => {
-  console.log(req.body);
-
-  res.json({
-    message: "Hello from the post products",
-    product: req.body,
-  });
-};
-
-const findProduct = (req, res) => {
-  const id = req.params.id;
-  console.log(req.params);
-
-  res.json({
-    message: "Hello from the get one products",
-    id,
-  });
-};
-
-const deleteProduct = (req, res) => {
-  const id = req.params.id;
-
-  res.json({
-    message: "Hello from the delete product",
-    id,
-  });
-};
-
 app.use("/api/v1/products", productRouter);
-
-// app.post("/api/v1/products", createProduct);
-
-// app.get("/api/v1/products/:id", findProduct);
-
-// app.delete("/api/v1/products/:id", deleteProduct);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000! 🐱‍🐉");
--- REPASO/cafeApp/src/routes/products.routes.js
+++ REPASO/cafeApp/src/routes/products.routes.js
@@ -17,8 +17,42 @@ const updateProduct = (req, res) => {
   });
 };
 
+const createProduct = (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    message: "Hello from the post products",
+    product: req.body,
+  });
+};
+
+const findProduct = (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+
+  res.json({
+    message: "Hello from the get one products",
+    id,
+  });
+};
+
+const deleteProduct = (req, res) => {
+  const id = req.params.id;
+
+  res.json({
+    message: "Hello from the delete product",
+    id,
+  });
+};
+
 router.get("/", findProducts);
 
+router.post("/", createProduct);
+
+router.get("/:id", findProduct);
+
 router.patch("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
