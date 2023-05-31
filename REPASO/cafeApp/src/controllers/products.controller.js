64f222ac2b2e7d852db55fa82703bb5817d7fec1
luismavlo <lmavendano1597@gmail.com>
2023-05-30 23:02:09 -0500

    next Commit

--- REPASO/cafeApp/src/controllers/products.controller.js
+++ REPASO/cafeApp/src/controllers/products.controller.js
@@ -1,9 +1,16 @@
-exports.findProducts = (req, res) => {
+const Product = require("../models/product.model");
+
+exports.findProducts = async (req, res) => {
   const time = req.requestTime;
+
+  const products = await Product.findAll();
 
   return res.json({
     requestTime: time,
-    message: "Hello from the get products",
+    results: products.length,
+    status: "success",
+    message: "Products found",
+    products,
   });
 };
 
@@ -16,14 +23,37 @@ exports.updateProduct = (req, res) => {
   });
 };
 
-exports.createProduct = (req, res) => {
-  const { name, image, ingredients, quantity, price, isNew, description } =
-    req.body;
+exports.createProduct = async (req, res) => {
+  try {
+    // PASO 1: OBTENER INFORMACION A CREAR DE LA REQ.BODY
+    const { name, image, ingredients, quantity, price, isNew, description } =
+      req.body;
 
-  return res.status(201).json({
-    message: "Hello from the post products",
-    product: req.body,
-  });
+    //PASO 2: CREAR EL PRODUCTO UTILIZANDO EL MODELO
+
+    const product = await Product.create({
+      name,
+      image,
+      ingredients,
+      quantity,
+      price,
+      isNew,
+      description,
+    });
+
+    // PASO 3: ENVIAR UNA RESPUESTA AL CLIENTE
+
+    return res.status(201).json({
+      message: "The product has been created!",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "fail",
+      message: "Something went very wrong!",
+    });
+  }
 };
 
 exports.findProduct = (req, res) => {
