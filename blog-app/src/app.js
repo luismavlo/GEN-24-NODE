204f8a01df97a0fcddca1f4932ec8ad3e75b7e27
luismavlo <lmavendano1597@gmail.com>
2023-06-08 22:10:25 -0500

    next commit

--- blog-app/src/app.js
+++ blog-app/src/app.js
@@ -2,6 +2,8 @@ const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+
 //routes
 const authRouter = require('./routes/auth.routes');
 const userRouter = require('./routes/users.routes');
@@ -16,5 +18,22 @@ app.use(morgan('dev'));
 //rutas
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  return next(
+    new AppError(`Cant find ${req.originalUrl} on this server!`, 404)
+  );
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
