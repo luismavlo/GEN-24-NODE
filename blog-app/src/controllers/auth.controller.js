71f534ce474b1e812fd615834cee6a3f781712e8
luismavlo <lmavendano1597@gmail.com>
2023-06-12 21:06:21 -0500

    next commit

--- blog-app/src/controllers/auth.controller.js
+++ blog-app/src/controllers/auth.controller.js
@@ -1,15 +1,31 @@
 const User = require('../models/users.model');
 const catchAsync = require('./../utils/catchAsync');
+const bcrypt = require('bcryptjs');
 
 exports.signup = catchAsync(async (req, res) => {
   const { name, email, password, description } = req.body;
 
-  const user = await User.create({ name, email, password, description });
+  const salt = await bcrypt.genSalt(12);
+  const encryptedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: encryptedPassword,
+    description,
+  });
 
   res.status(200).json({
     status: 'success',
     message: 'The user has been created',
-    user,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      description: user.description,
+      profileImgUrl: user.profileImgUrl,
+      role: user.role,
+    },
   });
 });
 
