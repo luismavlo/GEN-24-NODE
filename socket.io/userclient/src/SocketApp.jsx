e3a14b6a8e2289cbf2a55855c0492e2958c8ff9b
Luis Miguel <lmavendano1597@gmail.com>
2023-06-26 21:57:59 -0500

    next commit

--- socket.io/userclient/src/SocketApp.jsx
+++ socket.io/userclient/src/SocketApp.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { StudentAdd } from "./components/StudentAdd";
 import { StudentList } from "./components/StudentList";
 import { io } from "socket.io-client";
@@ -11,12 +12,45 @@ const connectSocketServer = () => {
 };
 
 export const SocketApp = () => {
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false); //offline por default
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("disconnect", () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("current-students", (students) => {
+      setStudents(students);
+    });
+  }, [socket]);
+
+  const vote = (id) => {
+    socket.emit("vote-student", id);
+  };
+
   return (
     <section className="container">
       <header className="alert">
         <h4>Services Status</h4>
-        <span className="text-success">Online</span>
-        <span className="text-danger">Offline</span>
+        {online ? (
+          <span className="text-success">Online</span>
+        ) : (
+          <span className="text-danger">Offline</span>
+        )}
 
         <h1>Students Node Gen24</h1>
         <hr />
@@ -24,7 +58,7 @@ export const SocketApp = () => {
 
       <article className="row">
         <div className="col-8">
-          <StudentList />
+          <StudentList data={students} vote={vote} />
         </div>
         <div className="col-4">
           <StudentAdd />
--- socket.io/userclient/src/components/StudentList.jsx
+++ socket.io/userclient/src/components/StudentList.jsx
@@ -1,4 +1,36 @@
-export const StudentList = () => {
+import { useEffect, useState } from "react";
+
+export const StudentList = ({ data, vote }) => {
+  const [students, setStudents] = useState(data);
+
+  useEffect(() => {
+    setStudents(data);
+  }, [data]);
+
+  const createRows = () => {
+    return students.map((student) => (
+      <tr key={student.id}>
+        <td>
+          <button
+            className="btn btn-outline-success"
+            onClick={() => vote(student.id)}
+          >
+            +1
+          </button>
+        </td>
+        <td>
+          <input type="text" className="form-control" value={student.name} />
+        </td>
+        <td>{student.votes}</td>
+        <td>
+          <button type="button" className="btn btn-outline-danger">
+            Borrar
+          </button>
+        </td>
+      </tr>
+    ));
+  };
+
   return (
     <>
       <h3>Estudiantes Actuales</h3>
@@ -12,32 +44,7 @@ export const StudentList = () => {
             <th>Borrar</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>+1</td>
-            <td>filed</td>
-            <td>field</td>
-            <td>borrar</td>
-          </tr>
-          <tr>
-            <td>+1</td>
-            <td>filed</td>
-            <td>field</td>
-            <td>borrar</td>
-          </tr>
-          <tr>
-            <td>+1</td>
-            <td>filed</td>
-            <td>field</td>
-            <td>borrar</td>
-          </tr>
-          <tr>
-            <td>+1</td>
-            <td>filed</td>
-            <td>field</td>
-            <td>borrar</td>
-          </tr>
-        </tbody>
+        <tbody>{createRows()}</tbody>
       </table>
     </>
   );
